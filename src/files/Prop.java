17dc14cddb0a55590096f2bd7d902bffc61c2da8
boufasimo1 <120329116+boufasimo1@users.noreply.github.com>
2023-10-24 22:25:47 -0500

    first commit

---
+++ src/files/Prop.java
@@ -0,0 +1,97 @@
+package files;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Random;
+
+public class Prop {
+
+	public static void main(String[] args) throws IOException {
+		Random rnd = new Random();
+		System.out.println(rnd.nextLong(99949845984941989L));
+		int[] a = {12,2,3,54,5,3,41,56};
+		int i =0;
+		int k =0;
+		for (int s : a) {
+			k=s;break;
+		}
+		System.out.println(k);
+
+//		Properties p=new Properties();
+//		FileInputStream fi=new FileInputStream("file\\test.properties");
+//		FileOutputStream fo=new FileOutputStream("file\\test.properties",true);
+//		p.load(fi);
+//		p.setProperty("aboudf", "1595");
+//		
+//		FileWriter s=new FileWriter("file\\test.properties",true);
+//		
+//		p.store(fo, null);
+//		System.out.println(p.get("aboudf"));
+	}
+//		}
+//		catch(Exception e) {System.out.println("hello");}
+		
+//		String path ="file/txt2.csv";
+//		File f= new File (path);
+//	
+//		-
+//		
+//		
+//		
+//		 FileWriter fw = new FileWriter (path);
+//		BufferedWriter bw = new BufferedWriter(fw);
+//		
+//		
+//		
+//		bw.write("Abdou,232423, sds");
+//		bw.newLine();
+//		bw.write("Abdou,324232, sds");
+//		bw.newLine();
+//		bw.write("Abdou,2423, sds");
+//		bw.newLine();
+//		bw.write("Abdou,2, sds");
+//		bw.close();//****************************************** *****************
+//		
+//		FileReader fr = new FileReader(f);
+//		BufferedReader br = new BufferedReader(fr);
+//		String g = br.readLine().replace(',', ' ');
+//		System.out.println(g);
+//		System.out.println(br.readLine());
+//		System.out.println(br.readLine());
+//		System.out.println(br.readLine());
+//		System.out.println(br.readLine());
+//		System.out.println(br.readLine());
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		Properties p=new Properties();
+//		FileInputStream fi=new FileInputStream("file\\test.properties");
+//		FileOutputStream fo=new FileOutputStream("file\\test.properties",true);
+//		p.load(fi);
+//		p.setProperty("aboudf", "1595");
+//		
+//		FileWriter s=new FileWriter("file\\test.properties",true);
+//		
+//		p.store(fo, null);
+//		System.out.println(p.get("aboudf"));
+
+	
+
+}
